36f5ed95d59b31ed39bfd08d50438f618286c689
Muhammet Cin <muhammetacin@gmail.com>
2023-03-25 17:52:49 +0100

    Pass Recipe data with property binding

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("A Test Recipe", "This is a simple test.", 
+    "https://images.pexels.com/photos/4792402/pexels-photo-4792402.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+    new Recipe("A Test Recipe 2", "This is a simple test 2.", 
     "https://images.pexels.com/photos/4792402/pexels-photo-4792402.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
   ];
 
